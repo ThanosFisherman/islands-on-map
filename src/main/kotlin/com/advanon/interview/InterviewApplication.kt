a24ec4743663bbf9c648593b6092706643ef670c
Thanos Psaridis <psaridis@gmail.com>
2018-07-11 21:01:19 +0300

    Initial commit - Sceleton project

---
+++ src/main/kotlin/com/advanon/interview/InterviewApplication.kt
@@ -0,0 +1,11 @@
+package com.advanon.interview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<InterviewApplication>(*args)
+}
