31dc647a4411c25d743b6ab0be733ee076e85167
Thanos Psaridis <psaridis@gmail.com>
2018-07-12 13:03:22 +0300

    RestClient to get Map from URL

---
+++ src/main/kotlin/com/advanon/interview/clients/RestClient.kt
@@ -0,0 +1,17 @@
+package com.advanon.interview.clients
+
+import com.advanon.interview.entities.MapEntity
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.stereotype.Component
+
+@Component
+class RestClient(val restTemplateBuilder: RestTemplateBuilder) {
+
+    private val url = "https://private-2e8649-advapi.apiary-mock.com/map"
+
+    fun getMap(): MapEntity {
+        val restTemplate = restTemplateBuilder.build()
+        val mapEntity = restTemplate.getForObject(url, MapEntity::class.java)
+        return mapEntity ?: throw RuntimeException("Couldn't get maps from url. Try again later")
+    }
+}
