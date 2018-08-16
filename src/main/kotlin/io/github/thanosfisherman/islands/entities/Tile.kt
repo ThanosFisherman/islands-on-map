ffb10eaec946ed4d52fcc9412921c91a12d2a026
Thanos Psaridis <psaridis@gmail.com>
2018-08-16 22:37:58 +0300

    Add Coords class (might come in handy later)

--- src/main/kotlin/io/github/thanosfisherman/islands/entities/Tile.kt
+++ src/main/kotlin/io/github/thanosfisherman/islands/entities/Tile.kt
@@ -1,11 +1,17 @@
 package io.github.thanosfisherman.islands.entities
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonRootName
 
 data class Tile(val x: Int, val y: Int, var type: TileType) {
-    val isLand: Boolean get() = type == TileType.LAND
+    val isLand: Boolean @JsonIgnore get() = type == TileType.LAND
+    val coords: Coords @JsonIgnore get() = Coords(x, y)
+
+    constructor(coords: Coords, type: TileType) : this(coords.x, coords.y, type)
 }
+
+data class Coords(val x: Int, val y: Int)
 
 @JsonRootName("type")
 enum class TileType {
