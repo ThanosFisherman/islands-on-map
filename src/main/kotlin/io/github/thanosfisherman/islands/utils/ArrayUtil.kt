7b7f5646bc8555b5e7cd5b0547c1da0346e8e2fd
Thanos Psaridis <psaridis@gmail.com>
2018-08-16 21:13:41 +0300

    Fix for build2dArray

--- src/main/kotlin/io/github/thanosfisherman/islands/utils/ArrayUtil.kt
+++ src/main/kotlin/io/github/thanosfisherman/islands/utils/ArrayUtil.kt
@@ -5,15 +5,16 @@ import io.github.thanosfisherman.islands.entities.Tile
 object ArrayUtil {
 
     fun build2dArray(tiles: List<Tile>): Array<Array<Tile>> {
+
+        //sort the coords first
         val tilesSorted = tiles.map { it.copy() }
                 .sortedWith(Comparator { o1, o2 -> if (compareValues(o1.x, o2.x) == 0) 0 else compareValues(o1.y, o2.y) })
-        val tiles2d = Array(tilesSorted[tilesSorted.size - 1].y) { _ -> Array(tilesSorted[tilesSorted.size - 1].x) { Tile(0, 0, "water") } }
-        var counter = 0
-        for (i in tiles2d.indices)
-            for (j in tiles2d[i].indices) {
-                tiles2d[i][j] = tiles[counter]
-                counter++
-            }
+
+        //create a 2d sea
+        val tiles2d = Array(tilesSorted[tilesSorted.size - 1].x + 1) { i -> Array(tilesSorted[tilesSorted.size - 1].y + 1) { j -> Tile(i, j, "water") } }
+
+        //add islands to 2d sea
+        tilesSorted.forEach { t -> tiles2d[t.x][t.y] = t }
 
         return tiles2d
     }
